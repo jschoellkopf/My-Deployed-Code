135bfbcd7f59ecbbb4b68788fa3734da1deb3d0a
jschoellkopf <j.schoellkopf59@gmail.com>
2022-08-30 09:25:53 -0700

    Liking Likes

---
+++ CodingBootcamp/Assignments/W5 Liking Likes/script.js
@@ -0,0 +1,19 @@
+var count1 = 9
+var count2 = 12
+var count3 = 9
+
+function add1Like() {
+    var likeElement = document.querySelector(".likeNbr")
+    count1 ++
+    likeElement.innerText = count1 + " like(s)"
+}
+function add1Like2() {
+    var likeElement = document.querySelector(".likeNbr2")
+    count2 ++
+    likeElement.innerText = count2 + " like(s)"
+}
+function add1Like3() {
+    var likeElement = document.querySelector(".likeNbr3")
+    count3 ++
+    likeElement.innerText = count3 + " like(s)"
+}
