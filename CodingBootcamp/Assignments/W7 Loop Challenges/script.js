e73bd1b2fa72d8cbc84a096647f3cced1d416218
jschoellkopf <j.schoellkopf59@gmail.com>
2022-09-15 09:54:33 -0700

    Loop Challenges

---
+++ CodingBootcamp/Assignments/W7 Loop Challenges/script.js
@@ -0,0 +1,19 @@
+for (var i =1; i<20; i +=2) {
+    console.log(i)
+}
+
+for (var i = 100; i>=0; i--) {
+    if (i % 3 == 0) {
+        console.log(i)
+    }
+}
+
+for ( var i = 4; i > -4 ; i -= 1.5) {
+    console.log(i)
+}
+
+var sum = 0
+for (var i = 0; i <= 100; i ++) {
+    sum += i
+}
+console.log(sum)
