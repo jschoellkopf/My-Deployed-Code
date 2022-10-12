3a600243e67f862f09f234aba4f8b5dff66f2e5b
jschoellkopf <j.schoellkopf59@gmail.com>
2022-10-12 08:48:17 -0700

    code wars and modularization

---
+++ CodingBootcamp/Assignments/W12 Python/modularizing/child.py
@@ -0,0 +1,14 @@
+import parent
+#print(locals())
+#prints 
+#{'__name__': '__main__', 
+# '__doc__': None, 
+# '__package__': None, 
+# '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x105e54e20>, 
+# '__spec__': None, 
+# '__annotations__': {}, 
+# '__builtins__': <module 'builtins' (built-in)>, 
+# '__file__': '/Users/jonas/Desktop/my_code/My-Deployed-Code/CodingBootcamp/Assignments/W12 Python/modularizing/child.py', 
+# '__cached__': None, 
+# 'parent': <module 'parent' from '/Users/jonas/Desktop/my_code/My-Deployed-Code/CodingBootcamp/Assignments/W12 Python/modularizing/parent.py'>
+# }
